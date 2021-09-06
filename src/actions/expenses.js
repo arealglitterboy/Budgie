ccfff5293fb79e0630b6724e952cf542fbd57ea4
Ben Brown <benwbrown1029@gmail.com>
2021-09-06 10:37:39 +0100

    Removed unnecessary files Restructured public directory

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,24 +1,25 @@
 import {v4 as uuid} from 'uuid';
 
+/**
+ * 
+ * @param {object} expense expense details. Optional object properties of description, note, date, currency and amount.
+ * @returns @type {expense} returns an expense object.
+ */
+// Deconstruct the input object, taking only the required properties, and initialising missing properties with default values.
 export const addExpense = (
-    {
-        description = '',
-        note = '',
-        date = 0,
-        currency = '€',
-        amount = 0
-    } = {}
+        { description = '', note = '', date = 0, currency = '€', amount = 0 } = {}
     ) => ({
-    type: 'ADD_EXPENSE',
-    expense: {
-        id: uuid(),
-        description,
-        note,
-        date,
-        currency,
-        amount
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: uuid(),
+            description,
+            note,
+            date,
+            currency,
+            amount
+        }
     }
-});
+);
 
 export const removeExpense = ({ id }) => ({ type: 'REMOVE_EXPENSE', id });
 
