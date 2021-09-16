50f27207e068bdeb78e7cce5b09134ee2643b0fa
Ben Brown <benwbrown1029@gmail.com>
2021-09-16 21:24:38 +0100

    Worked on ExpenseList component. Created styling for ExpenseList and ExpenseListItem. Altered styling for ExpenseForm. Added new Budgie colour scheme. Added new Site fonts. Recreated ExpenseListItem structure. Added new Icon field to ExpenseListItem. Added project concepts to README.md.

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-# Expensify
+# Budgie
 
 ## `arealglitterboy`
 
-### Track your expenditure, compare purchases and manage your budget!
 
-A simple react based web application used for creating, searching, comparing and filtering personal expenses. Currently in development.
+### Description
+
+
+Budgie is a react based web app for managing personal expenses and budgeting. By offering a suite of expense tracking, visual budgeting tools, sharable savings goals, and more, Budgie offers a simple and straight forward way to save money and analyse areas where you might be over spending.
+
+
 
 #### Goals
+
+
 - [ ] Add [Chart.js]() to graph expenses
 - [x] Add testing framework
 - [ ] Add styling
@@ -14,3 +20,61 @@ A simple react based web application used for creating, searching, comparing and
 - [ ] Develop live version
 - [ ] Get screenshots for GitHub
 - [ ] Create branding/aesthetic for the web app
+
+
+### Ideas
+
+
+#### Components
+
+
+##### Countdowns
+
+
+* Could be used as savings countdown, you set a target amount and a target date and budgie will calculate how much money you need to deposit per day or per week.
+  * When you miss a deposit, it will just recalculate to be a little bit more.
+  * Maybe could be set to strict or lax amounts, ie. saving for a holiday would be hopeful for an amount, but less or more is ok, whereas saving for a car needs to be a set amount.
+
+
+
+##### Bins
+
+* Bins that show how much has been spent in certain areas.
+* Could also be used for saving, ie. you can put money into bins which shows that you are comfortable spending $x$ amount on this specific thing(s).
+
+
+
+##### Budget Bands
+
+* Visual interactive graph that allows you to set the general proportion of your money on.
+  * Could just be resizable `<div>`s with icons and colours
+  * When you make a payment, then it will be added to the graph with its proportion of your total amount of money.
+  * If you spent more on food than you were wanting to, it will burst its bounds, give a percentage larger than expected, ie. $\color{red}\sf+20\%$.
+
+
+
+##### Expenses & Credits
+
+* Merge expensify expense list with a credits list so that you can show all money going in and out.
+
+
+
+#### Functionality
+
+##### CSV Export
+
+* Click a button and get a descriptive run down of your budgeting
+
+
+
+##### PDF Export
+
+* Click a button and get a visual breakdown of your budgeting
+
+
+
+##### Share Target
+
+* Maybe offer the ability to share a bin with someone.
+* Creating a share link will make it available to anyone with a link.
+  * Has option to send money to you to help reach goal?
--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -1,32 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const dateOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
+const today = new Date();
+
+const times = {
+    year:   31536000000, //(1000 * 60 * 60 * 24 * 365)
+    month:  2419200000,  //(1000 * 60 * 60 * 24 * 7 * 4)
+    week:   604800000,   //(1000 * 60 * 60 * 24 * 7)
+    day:    86400000,    //(1000 * 60 * 60 * 24)
+    hour:   3600000,     //(1000 * 60 * 60)
+    minute: 60000,       //(1000 * 60)
+    second: 1000,        //(1000)
 };
 
-export const ExpenseListItem = ({ id, description, note, currency, amount, date }) => {
-    const time = new Date(date);
-    return (
-        <article className="expense-list__item">
-            <header className="expense-list__item__header">
-                <h4 className="expense-list__item__header__description"><Link to={`/edit/${id}`} className="expense-list__item__header__description--link">{description}</Link></h4> 
-                <time className="expense-list__item__header__date" dateTime={`${time.toUTCString()}`}>
-                    {time.toLocaleString('en-IE', dateOptions)}
-                </time>
-            </header>
-            
-            {(note) && <section className="expense-list__item__note"><p>{note}</p></section>}
-            
-            <section className="expense-list__item__amount">
-                <data value={amount}>
+const formatDifference = (time, unit, timeFrame) => (time + " " + unit + ((time != 1) ? 's' : '') + ' ' + timeFrame);
+
+function findDifference(date) {
+    const difference = Math.abs(date - today);
+    const key = Object.keys(times).find((time) => (difference >= times[time]));
+    return (key) ? formatDifference(Math.floor(difference/times[key]), key, ((date > today) ? 'till' : 'ago')) : "Now";
+}
+
+
+export default class ExpenseListItem extends React.Component {
+    handleClick = (e) => {
+        console.log(this);
+        console.log(e);
+    };
+    
+    render() {
+        const { description, note, date, amount, currency, id} = this.props;
+        const time = new Date(date);
+        
+        return (
+            <article className="expense-item" onClick={this.handleClick}>
+                <img src="" alt="icon" className="expense-item__icon" />
+                
+                <section className="expense-item__info">
+                    <time className="expense-item__info__date" dateTime={`${time.toUTCString()}`}>{findDifference(date)}</time>
+                    <h5 className="expense-item__info__description">{description}</h5> 
+                </section>
+                
+                <data className="expense-item__amount" value={amount}>
                     {(amount/100).toLocaleString('en-US', { style: "currency", currency })}
                 </data>
-            </section>
-        </article>
-    )
-};
-
-export default ExpenseListItem;
+                
+                <footer className="expense-item__footer expense-item__footer--inactive">
+                    {(note) && <p>{note}</p>}
+                    <Link to={`/edit/${id}`} className="expense-item__footer__edit">EDIT</Link>
+                </footer>
+            </article>
+        );
+    }
+}
