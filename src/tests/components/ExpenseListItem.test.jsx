5a019c51048e52ceaeeb43f2fe4575b417ee55a4
Ben Brown <62847149+arealglitterboy@users.noreply.github.com>
2021-09-25 22:43:33 +0100

    Added `today` attribute to `ExpenseListItem` tests

--- src/tests/components/ExpenseListItem.test.jsx
+++ src/tests/components/ExpenseListItem.test.jsx
@@ -5,11 +5,11 @@ import { expenses } from "../fixtures/expenses.fixture";
 import ExpenseListItem from "../../components/ExpenseListItem";
 
 test('should render Expense List Item with expense details', () => {
-    const wrapper = shallow(<ExpenseListItem { ...expenses[0] } />);
+    const wrapper = shallow(<ExpenseListItem { ...expenses[0] } today={new Date('2021-09-12')} />);
     expect(wrapper).toMatchSnapshot();
 });
 
 test('should render Expense List Item with expense details and different currency', () => {
-    const wrapper = shallow(<ExpenseListItem { ...expenses[2] } />)
+    const wrapper = shallow(<ExpenseListItem { ...expenses[2] } today={new Date('2021-09-12')} />)
     expect(wrapper).toMatchSnapshot();
 });
