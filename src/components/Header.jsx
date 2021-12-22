e2663eb9d7eac3850e94f003c4ba922f239bdc6a
Ben Brown <62847149+arealglitterboy@users.noreply.github.com>
2021-12-22 13:44:51 +0000

    🎨 Styled page header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = (props) => (
-    <header className="page-header">
-        {/* <img src="/images/budgie--v3.svg" alt="Budgie Logo" className="page-header__logo" /> */}
-        <h2 className="page-header__title">Budgie</h2>
-        <nav className="page-header__nav">
-            <NavLink to="/" className="page-header__nav__link" exact activeClassName="page-header__nav__link--active">Dashboard</NavLink>
-            <NavLink to="/create" className="page-header__nav__link" activeClassName="page-header__nav__link--active">Create</NavLink>
-            <NavLink to="/contacts" className="page-header__nav__link" activeClassName="page-header__nav__link--active">Contacts</NavLink>
-            <NavLink to="/help" className="page-header__nav__link" activeClassName="page-header__nav__link--active">Help</NavLink>
-        </nav>
+    <header className="page-header--outer">
+        <div className="page-header">
+            <h2 className="page-header__title"><NavLink className="page-header__title--link" to="/" exact>Budgie</NavLink></h2>
+            <nav className="page-header__nav">
+                <NavLink to="/" className="page-header__nav__link" exact activeClassName="page-header__nav__link--active">Dashboard</NavLink>
+                <NavLink to="/create" className="page-header__nav__link" activeClassName="page-header__nav__link--active">Create</NavLink>
+                <NavLink to="/contacts" className="page-header__nav__link" activeClassName="page-header__nav__link--active">Contacts</NavLink>
+                <NavLink to="/help" className="page-header__nav__link" activeClassName="page-header__nav__link--active">Help</NavLink>
+            </nav>
+        </div>
     </header>
 );
 
